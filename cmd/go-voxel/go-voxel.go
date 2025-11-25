99071254d0ffecd38116d69fcd0de61b3607a007
Zachary Heskett <zheskett@gmail.com>
2025-11-25 17:40:25 -0500

    moved rendering into render.go

--- cmd/go-voxel/go-voxel.go
+++ cmd/go-voxel/go-voxel.go
@@ -3,15 +3,8 @@ package main
 import (
 	"runtime"
 
-	"github.com/go-gl/gl/v3.3-core/gl"
 	"github.com/go-gl/glfw/v3.3/glfw"
 	"github.com/zheskett/go-voxel/internal/render"
-)
-
-const (
-	WindowTitle  = "Go Voxel"
-	WindowWidth  = render.TextureWidth * 4
-	WindowHeight = render.TextureHeight * 4
 )
 
 func init() {
@@ -34,38 +27,17 @@ func main() {
 	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
 	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
 
-	window, err := glfw.CreateWindow(WindowWidth, WindowHeight, WindowTitle, nil, nil)
+	window, err := glfw.CreateWindow(render.WindowWidth, render.WindowHeight, render.WindowTitle, nil, nil)
 	if err != nil {
 		panic(err)
 	}
 	window.MakeContextCurrent()
-	gl.Init()
 
-	var fbo, renderTexture uint32
-	gl.GenFramebuffers(1, &fbo)
-	gl.BindFramebuffer(gl.FRAMEBUFFER, fbo)
-
-	gl.GenTextures(1, &renderTexture)
-	gl.BindTexture(gl.TEXTURE_2D, renderTexture)
-	gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGBA, render.TextureWidth, render.TextureHeight, 0, gl.RGBA, gl.UNSIGNED_BYTE, nil)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
-	gl.FramebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, renderTexture, 0)
+	rm := render.Init()
 
 	for !window.ShouldClose() {
 		// Do stuff here
-
-		pixels := render.Render()
-
-		gl.BindFramebuffer(gl.FRAMEBUFFER, fbo)
-		gl.Viewport(0, 0, render.TextureWidth, render.TextureHeight)
-
-		gl.BindTexture(gl.TEXTURE_2D, renderTexture)
-		gl.TexSubImage2D(gl.TEXTURE_2D, 0, 0, 0, int32(render.TextureWidth), int32(render.TextureHeight), gl.RGBA, gl.UNSIGNED_BYTE, gl.Ptr(pixels))
-
-		gl.BindFramebuffer(gl.READ_FRAMEBUFFER, fbo)
-		gl.BindFramebuffer(gl.DRAW_FRAMEBUFFER, 0)
-		gl.BlitFramebuffer(0, 0, render.TextureWidth, render.TextureHeight, 0, 0, WindowWidth, WindowHeight, gl.COLOR_BUFFER_BIT, gl.NEAREST)
+		rm.Render()
 
 		window.SwapBuffers()
 		glfw.PollEvents()
--- internal/render/render.go
+++ internal/render/render.go
@@ -1,20 +1,68 @@
 // Package render provides a renderer for the voxels
 package render
 
+import (
+	"github.com/go-gl/gl/v3.3-core/gl"
+)
+
 const (
 	TextureWidth  = 320
 	TextureHeight = 240
+	WindowWidth   = TextureWidth * 4
+	WindowHeight  = TextureHeight * 4
+	WindowTitle   = "Go Voxel"
 )
 
+// Pixles contains the data for each pixel on the screen.
+// Every pixel if 4 bytes, RGBA
 type Pixels []byte
 
-func Render() Pixels {
-	pixels := make(Pixels, TextureWidth*TextureHeight*4)
-	for i := range pixels {
-		pixels[i] = 255
+// RenderManager contains state for the rendering
+type RenderManager struct {
+	renderTexture uint32
+	fbo           uint32
+	pixels        Pixels
+}
+
+// Init initializes the render manager
+// and initializes the opengl context
+func Init() *RenderManager {
+	gl.Init()
+
+	var rm RenderManager = RenderManager{}
+
+	gl.GenFramebuffers(1, &rm.fbo)
+	gl.BindFramebuffer(gl.FRAMEBUFFER, rm.fbo)
+
+	gl.GenTextures(1, &rm.renderTexture)
+	gl.BindTexture(gl.TEXTURE_2D, rm.renderTexture)
+	gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGBA, TextureWidth, TextureHeight, 0, gl.RGBA, gl.UNSIGNED_BYTE, nil)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+	gl.FramebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, rm.renderTexture, 0)
+
+	rm.pixels = make(Pixels, TextureWidth*TextureHeight*4)
+
+	return &rm
+}
+
+// Render renders the current state
+// It should be called each frame
+func (rm *RenderManager) Render() {
+	for i := range rm.pixels {
+		rm.pixels[i] = 255
 		if i&0x3 == 0x1 {
-			pixels[i] = 0
+			rm.pixels[i] = 0
 		}
 	}
-	return pixels
+
+	gl.BindFramebuffer(gl.FRAMEBUFFER, rm.fbo)
+	gl.Viewport(0, 0, TextureWidth, TextureHeight)
+
+	gl.BindTexture(gl.TEXTURE_2D, rm.renderTexture)
+	gl.TexSubImage2D(gl.TEXTURE_2D, 0, 0, 0, int32(TextureWidth), int32(TextureHeight), gl.RGBA, gl.UNSIGNED_BYTE, gl.Ptr(rm.pixels))
+
+	gl.BindFramebuffer(gl.READ_FRAMEBUFFER, rm.fbo)
+	gl.BindFramebuffer(gl.DRAW_FRAMEBUFFER, 0)
+	gl.BlitFramebuffer(0, 0, TextureWidth, TextureHeight, 0, 0, WindowWidth, WindowHeight, gl.COLOR_BUFFER_BIT, gl.NEAREST)
 }
