caefb52adba9d2206b7c23fab34b0c03091c9fcc
Zachary Heskett <zheskett@gmail.com>
2025-12-24 19:16:03 -0500

    changed test-noise

--- cmd/test-noise/test-noise.go
+++ cmd/test-noise/test-noise.go
@@ -3,17 +3,19 @@ package main
 import (
 	_ "fmt"
 
+	"github.com/chewxy/math32"
 	"github.com/zheskett/go-voxel/internal/noise"
 )
 
 func main() {
-	per, err := noise.GenPerlin3D(256)
+	perlin, err := noise.GenPerlin3D(256)
 	if err != nil {
 		panic(err)
 	}
-	noise.Draw(&per, "noise.png", 2048, 0, 0.01)
-	fbm := noise.FBM3D{Source: &per, Octaves: 8, H: 0.5}
-	noise.Draw(&fbm, "FBM.png", 2048, 0, 0.01)
-	applied := noise.FnNoise3D{Source: &fbm, Fn: noise.CuRat}
-	noise.Draw(&applied, "applied.png", 2048, 0, 0.01)
+	fbm := &noise.FBM3D{Source: &perlin, Octaves: 7, H: 1}
+	fn := &noise.FnNoise3D{Source: fbm, Fn: noise.CuRat}
+	fn = &noise.FnNoise3D{Source: fn, Fn: func(t float32) float32 {
+		return 0.5 * (math32.Pow(t, 14) + math32.Pow(t, 0.75))
+	}}
+	noise.Draw(fn, "applied.png", 2048, 0, 0.01)
 }
